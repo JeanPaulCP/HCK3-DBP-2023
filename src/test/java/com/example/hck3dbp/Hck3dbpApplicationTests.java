3a4b65b1e9dd30d91166fa9b6d91ed85fe0de98f
Paul <jean.cuzcano@utec.edu.pe>
2023-10-23 15:08:24 -0500

    Primer avance

---
+++ src/test/java/com/example/hck3dbp/Hck3dbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hck3dbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hck3dbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
